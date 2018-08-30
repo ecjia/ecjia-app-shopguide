b816bd33f5d2fac31691826f8e917659be47788c
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-31 00:01:25 +0800

    指定ServiceProvider的路径

--- classes/ShopguideServiceProvider.php
+++ classes/ShopguideServiceProvider.php
@@ -9,7 +9,7 @@ class ShopguideServiceProvider extends  AppParentServiceProvider
     
     public function boot()
     {
-        $this->package('ecjia/app-shopguide', null, dirname(__DIR__));
+        $this->package('ecjia/app-shopguide');
     }
     
     public function register()
